e6228ad2417b6f4a8ee7bf4c03e103624fff1547
galibe_s <stephane.galibert@epitech.eu>
2017-01-31 19:17:30 +0100

    Initial commit

---
+++ app/src/main/java/fr/epicture/epicture/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package fr.epicture.epicture.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fr.epicture.epicture.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}

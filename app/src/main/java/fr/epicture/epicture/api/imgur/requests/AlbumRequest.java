38796ca2323417a5554b096ce74695b163916769
Benjamin Rascol <benjamin.rascol@ascentia.fr>
2017-02-24 19:01:47 +0100

    debug test

--- app/src/main/java/fr/epicture/epicture/api/imgur/requests/AlbumRequest.java
+++ app/src/main/java/fr/epicture/epicture/api/imgur/requests/AlbumRequest.java
@@ -3,16 +3,15 @@ package fr.epicture.epicture.api.imgur.requests;
 import android.content.Context;
 import android.support.annotation.NonNull;
 
-import fr.epicture.epicture.interfaces.LoadImageElementInterface;
 import fr.epicture.epicture.interfaces.LoadTextInterface;
-import fr.epicture.epicture.requests.TextRequest;
+import fr.epicture.epicture.requests.TextPostRequest;
 
-public class AlbumRequest extends TextRequest {
+public class AlbumRequest extends TextPostRequest {
 
-    private static final String URL = "https://api.imgur.com/3/account/%s/albums/";
+    private static final String URL = "https://api.imgur.com/3/account/%s/albums/%d";
 
     public AlbumRequest(@NonNull Context context, String username, String accessToken, LoadTextInterface listener) {
-        super(context, String.format(URL, username), listener);
+        super(context, String.format(URL, username, 1), listener);
         addHeader("Authorization", "Bearer " + accessToken);
     }
 }
--- app/src/main/java/fr/epicture/epicture/api/imgur/requests/UserInformationRequest.java
+++ app/src/main/java/fr/epicture/epicture/api/imgur/requests/UserInformationRequest.java
@@ -4,22 +4,22 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 
 import fr.epicture.epicture.interfaces.LoadTextInterface;
-import fr.epicture.epicture.requests.TextPostRequest;
+import fr.epicture.epicture.requests.TextRequest;
 
-public class UserInformationRequest extends TextPostRequest {
+public class UserInformationRequest extends TextRequest {
 
     // ========================================================================
     // STATIC FIELDS
     // ========================================================================
 
-    private static final String BASE_URL = "https://api.imgur.com/3/account/";
+    private static final String URL = "https://api.imgur.com/3/account/me";
 
     // ========================================================================
     // CONSTRUCTOR
     // ========================================================================
 
     public UserInformationRequest(@NonNull Context context, String username, String accessToken, LoadTextInterface listener) {
-        super(context, BASE_URL + username, listener);
+        super(context, URL, listener);
         addHeader("Authorization", "Bearer " + accessToken);
     }
 }
